42be507c1745d761f0469f1861ae7359731e9f10
Benedikt Ritter <beneritter@gmail.com>
2022-05-31 22:01:01 +0200

    Add examples for environment and system property overloads

--- samples/basics/gradle-build-logic/my-build-parameters/build.gradle.kts
+++ samples/basics/gradle-build-logic/my-build-parameters/build.gradle.kts
@@ -4,12 +4,19 @@ plugins {
 
 buildParameters {
     pluginId("de.benediktritter.build-params")
-    parameter("dbHost") {
+    string("dbHost") {
         defaultValue.set("localhost")
         description.set("Define the database host")
     }
-    parameter("tomcatHome") {
+    string("tomcatHome") {
         defaultValue.set("/tmp/tomcat")
         description.set("Define the installation directory of the local Tomcat server")
+    }
+    bool("ci") {
+        fromEnvironmentVariable() // -> -Pci=true, env var CI
+    }
+    bool("local") {
+        fromEnvironmentVariable("LOCAL_RUN") // -> -Plocal=true, env var LOCAL_RUN
+        fromSystemProperty() // -> -Dlocal=true
     }
 }
