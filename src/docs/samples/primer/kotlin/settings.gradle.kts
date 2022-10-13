cc33a9f37703e02662504203963e9065a7b2df02
Jendrik Johannes <jendrik.johannes@gmail.com>
2022-10-13 11:44:02 +0200

    Use pluginManagement {} to include gradle/plugins build

--- src/docs/samples/primer/kotlin/settings.gradle.kts
+++ src/docs/samples/primer/kotlin/settings.gradle.kts
@@ -1,1 +1,24 @@
-pluginManagement.includeBuild("gradle/plugins")
+pluginManagement {
+    includeBuild("gradle/plugins")
+}
+
+plugins {
+    id("build-parameters")
+}
+
+dependencyResolutionManagement {
+    repositories {
+        maven {
+            url = uri("https://repo.my-company.com")
+            credentials {
+                // username has a default and is therefore of type String
+                username = the<BuildParametersExtension>().deployment.username
+                // [Groovy DSL] buildParameters.deployment.username
+
+                // password does not have a default and is therefore of type Provider<String>
+                password = the<BuildParametersExtension>().deployment.password.get()
+                // [Groovy DSL] buildParameters.deployment.password.get()
+            }
+        }
+    }
+}
