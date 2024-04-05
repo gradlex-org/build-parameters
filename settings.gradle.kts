942b709a98759cf7e5472553389e51d2130451fe
Benedikt Ritter <benedikt@gradle.com>
2024-04-05 16:00:56 +0200

    Update to latest CCUD Gradle plugin release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 
 plugins {
     id("com.gradle.develocity") version "3.17"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.13"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.0"
     id("gradlexbuild.build-parameters")
 }
 
