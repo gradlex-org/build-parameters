ed681c394fb7d8281ab59dacc72ee7b65f09f2d5
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-10-20 08:23:32 +0200

    Migrate build to shared convention plugins setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,28 +1,5 @@
-import buildparameters.BuildParametersExtension
+pluginManagement { includeBuild("build/plugin-publish-conventions") }
+
+plugins { id("org.gradlex.internal.gradlex-build-conventions") version "0.7" }
 
 rootProject.name = "build-parameters"
-
-pluginManagement {
-    includeBuild("gradle/plugins")
-}
-
-plugins {
-    id("com.gradle.develocity") version "4.2.2"
-    id("com.gradle.common-custom-user-data-gradle-plugin") version "2.4.0"
-    id("gradlexbuild.build-parameters")
-}
-
-dependencyResolutionManagement {
-    repositories.gradlePluginPortal()
-}
-
-develocity {
-    buildScan {
-        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
-        termsOfUseAgree = "yes"
-
-        // required to bind this to a local variable for configuration cache compatibility
-        val isCi = the<BuildParametersExtension>().ci
-        publishing.onlyIf { isCi }
-    }
-}
