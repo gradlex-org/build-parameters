93324c67bff19a40d7c7ebe41f5fe37b933dcc71
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-10-30 14:44:08 +0100

    Format code

--- src/main/java/org/gradlex/buildparameters/BuildParametersPlugin.java
+++ src/main/java/org/gradlex/buildparameters/BuildParametersPlugin.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2022 the GradleX team.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// SPDX-License-Identifier: Apache-2.0
 package org.gradlex.buildparameters;
 
 import org.gradle.api.Plugin;
@@ -44,11 +29,15 @@ public class BuildParametersPlugin implements Plugin<Project> {
         BuildParametersExtension extension =
                 project.getExtensions().create("buildParameters", BuildParametersExtension.class, gradlePlugins);
 
-        TaskProvider<PluginCodeGeneration> task = project.getTasks().register("generatePluginCode", PluginCodeGeneration.class, t -> {
-            t.setDescription("Generates code for compile-safe access to build parameters.");
-            t.getBaseGroup().convention(extension);
-            t.getOutputDirectory().convention(project.getLayout().getBuildDirectory().dir("generated/sources/build-parameters-plugin/java/main"));
-        });
+        TaskProvider<PluginCodeGeneration> task = project.getTasks()
+                .register("generatePluginCode", PluginCodeGeneration.class, t -> {
+                    t.setDescription("Generates code for compile-safe access to build parameters.");
+                    t.getBaseGroup().convention(extension);
+                    t.getOutputDirectory()
+                            .convention(project.getLayout()
+                                    .getBuildDirectory()
+                                    .dir("generated/sources/build-parameters-plugin/java/main"));
+                });
         SourceSetContainer sourceSets = project.getExtensions().getByType(SourceSetContainer.class);
         SourceSet main = sourceSets.getByName("main");
         main.getJava().srcDir(task.flatMap(PluginCodeGeneration::getOutputDirectory));
@@ -65,7 +54,8 @@ public class BuildParametersPlugin implements Plugin<Project> {
         if (gradle.getParent() == null) {
             return acc;
         } else {
-            return getBuildPath(gradle.getParent(), ":" + gradle.getRootProject().getName() + acc);
+            return getBuildPath(
+                    gradle.getParent(), ":" + gradle.getRootProject().getName() + acc);
         }
     }
 }
