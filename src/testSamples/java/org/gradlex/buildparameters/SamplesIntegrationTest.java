615bdaa4d307f0739a04d446a6f91d11b9780b09
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-10-30 15:54:03 +0100

    Align samples test setup with the setup in other repositories

--- src/testSamples/java/org/gradlex/buildparameters/SamplesIntegrationTest.java
+++ src/testSamples/java/org/gradlex/buildparameters/SamplesIntegrationTest.java
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.gradlex.buildparameters;
 
+import org.gradle.exemplar.test.runner.SampleModifiers;
 import org.gradle.exemplar.test.runner.SamplesRoot;
 import org.gradle.exemplar.test.runner.SamplesRunner;
 import org.junit.runner.RunWith;
 
 @RunWith(SamplesRunner.class)
 @SamplesRoot("src/docs/snippets")
+@SampleModifiers(SamplesIntegrationTestModifier.class)
 public class SamplesIntegrationTest {}
